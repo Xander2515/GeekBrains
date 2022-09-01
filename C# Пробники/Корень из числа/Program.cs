ea712ac3083401046dfd2e9e5fa7db65e8b53211
Xander2515 <111518086+Xander2515@users.noreply.github.com>
2022-09-01 23:06:19 +0300

    Корень работает

---
+++ C# Пробники/Корень из числа/Program.cs
@@ -0,0 +1,34 @@
+﻿double Root(double a, int n)
+{
+    double Exp(double x, int n)
+    {
+        double result=1;
+
+        for (int i=0; i < n; i++)
+        {
+            result = result * x;
+        }
+
+        return result;
+    }
+
+    double result = 0;
+    
+    for (int i=0; i < 5; i++)
+    {
+        double b = 0;
+        while(b <= a)
+        {
+            result=result + 1/Exp(10, i);
+            b=1;
+            for (int j=0; j < n; j++)
+            {
+                b=b*result;
+            }
+        }
+        result=result-1/Exp(10, i);
+    }
+    return result;
+}
+
+Console.Write(Root(Convert.ToDouble(Console.ReadLine()), Convert.ToInt32(Console.ReadLine())));

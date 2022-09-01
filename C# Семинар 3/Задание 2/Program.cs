e4ae1361fddb00ff63354fde1cdf627b0eda06b5
Xander2515 <111518086+Xander2515@users.noreply.github.com>
2022-09-01 19:33:40 +0300

    Переделал задание 2 с массивами

--- C# Семинар 3/Задание 2/Program.cs
+++ C# Семинар 3/Задание 2/Program.cs
@@ -1,17 +1,31 @@
-﻿double Distance(double ax, double ay, double az, double bx, double by, double bz)
+﻿double Distance(double[] dotA, double[] dotB)
 {
-    return Math.Sqrt(Math.Pow(ax-bx, 2) + Math.Pow(ay-by, 2) + Math.Pow(az-bz, 2));
+    double result=0;
+
+    for (int i=0; i < 3; i++)
+    {
+        result = result + Math.Pow(dotA[i]-dotB[i], 2);
+    }
+
+    return Math.Sqrt(result);
 }
 
 Console.WriteLine("Введите координаты первой точки");
-double ax = Convert.ToDouble(Console.ReadLine());
-double ay = Convert.ToDouble(Console.ReadLine());
-double az = Convert.ToDouble(Console.ReadLine());
+
+double[] dotA = new double[3];
+double[] dotB = new double[3];
+
+for (int i=0; i < 3; i++)
+{
+    dotA[i] = Convert.ToDouble(Console.ReadLine());
+}
 
 Console.WriteLine("Введите координаты второй точки");
-double bx = Convert.ToDouble(Console.ReadLine());
-double by = Convert.ToDouble(Console.ReadLine());
-double bz = Convert.ToDouble(Console.ReadLine());
+
+for (int i=0; i < 3; i++)
+{
+    dotB[i] = Convert.ToDouble(Console.ReadLine());
+}
 
 Console.WriteLine("Расстояние");
-Console.WriteLine(Distance(ax, ay, az, bx, by, bz));
+Console.WriteLine(Distance(dotA, dotB));

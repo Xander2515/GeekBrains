4138dea4e963173c177491afd589c6ed33bd1a41
Xander2515 <111518086+Xander2515@users.noreply.github.com>
2022-09-11 01:05:08 +0300

    Семинар 6

---
+++ C# Семинар 6/Задание 1/Program.cs
@@ -0,0 +1,38 @@
+﻿float[] GetNumbers()
+{
+    Console.WriteLine("Введите числа, разделённые знаком |");
+
+    Console.WriteLine();
+
+    string a = Console.ReadLine();
+
+    string[] b = a.Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+    float[] result = new float[b.Length];
+
+    for (int i=0; i<b.Length; i++)
+        result[i] = Convert.ToSingle(b[i]);
+
+    return result;
+}
+
+int CountDigitsAboveZero(float[] a)
+{
+    int result = 0;
+
+    foreach(float i in a)
+    if (i>0)
+        result++;
+
+    return result;
+}
+
+Console.WriteLine();
+
+int b = CountDigitsAboveZero(GetNumbers());
+
+Console.WriteLine();
+
+Console.WriteLine($"Введено положительных чисел: {b}");
+
+Console.WriteLine();
---
+++ C# Семинар 6/Задание 2/Program.cs
@@ -0,0 +1,26 @@
+﻿float[] CountPoint (float k1, float b1, float k2, float b2)
+{
+float[] result = new float[2];
+result[0] = (b1-b2) / (k2 - k1);
+result[1] = result[0] * k1 + b1;
+return result;
+}
+
+Random rnd = new Random();
+
+float k1 = rnd.NextSingle()*10;
+float b1 = rnd.NextSingle()*100;
+float k2 = rnd.NextSingle()*10;
+float b2 = rnd.NextSingle()*100;
+
+Console.WriteLine();
+
+Console.WriteLine($"y = {k1} * x + {b1} | y = {k2} * x + {b2}");
+
+Console.WriteLine();
+
+float[] f = CountPoint(k1,b1,k2,b2);
+
+Console.WriteLine($"( {f[0]} | {f[1]} )");
+
+Console.WriteLine();

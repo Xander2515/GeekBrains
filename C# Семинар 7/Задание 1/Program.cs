44405a549581abffadd8e892c27e7b64a737cc1b
Xander2515 <111518086+Xander2515@users.noreply.github.com>
2022-09-16 19:39:58 +0300

    Семинар 7

---
+++ C# Семинар 7/Задание 1/Program.cs
@@ -0,0 +1,30 @@
+﻿float[,] GetArray(int m, int n)
+{
+    float[,] result = new float[m, n];
+    Random rnd = new Random();
+
+    for (int i=0; i<m; i++)
+        for (int j=0; j<n; j++)
+            result[i, j] = rnd.NextSingle()*20-10;
+    
+    return result;
+}
+
+void PrintArray(float[,] a)
+{
+    for (int i=0; i<a.GetLength(0); i++)
+    {
+
+        for (int j=0; j<a.GetLength(1); j++)
+            Console.Write($"| {a[i, j]} ");
+            
+        Console.WriteLine("|");
+    }
+}
+
+Console.WriteLine("Введите размерность массива");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+PrintArray(GetArray(a, b));
---
+++ C# Семинар 7/Задание 2/Program.cs
@@ -0,0 +1,47 @@
+﻿int[,] GetArray(int m, int n)
+{
+    int[,] result = new int[m, n];
+    Random rnd = new Random();
+
+    for (int i=0; i<m; i++)
+        for (int j=0; j<n; j++)
+            result[i, j] = rnd.Next(0, 10);
+    
+    return result;
+}
+
+void PrintArray(int[,] a)
+{
+    for (int i=0; i<a.GetLength(0); i++)
+    {
+        for (int j=0; j<a.GetLength(1); j++)
+            Console.Write($"| {a[i, j]} ");
+        
+        Console.WriteLine("|");
+    }
+}
+
+Console.WriteLine("Введите размерность массива");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = GetArray(a, b);
+
+PrintArray(arr);
+
+string FindNumber(int[,] a, int b)
+{
+    for (int i=0; i<a.GetLength(0); i++)
+        for (int j=0; j<a.GetLength(1); j++)
+            if(a[i, j] == b)
+                return $"{i} | {j}";
+    
+    return "Такого числа нет";
+}
+
+Console.WriteLine("Введите искомое число");
+
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console. WriteLine(FindNumber(arr, x));

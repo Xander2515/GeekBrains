f5a07beafe22c31007d8ce73db129fbaf6a073c1
Xander2515 <111518086+Xander2515@users.noreply.github.com>
2022-09-20 00:05:19 +0300

    Семинар 8

--- C# Семинар 8/Задача 62/Program.cs
+++ C# Семинар 8/Задача 62/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] GetArray(int m, int s)
+{
+    int[,] res = new int[m, m];
+    int n = (m+1)/2; //количество проходов по периметру
+    for (int i=0; i<n; i++)
+    {
+        for (int a=i; a<m-i; a++)
+        {res[i,a] = s; s++;}
+        for (int a=i+1; a<m-i; a++)
+        {res[a,m-i-1] = s; s++;}
+        for (int a=m-i-2; a>=i; a--)
+        {res[m-i-1,a] = s; s++;}
+        for (int a=m-i-2; a>=i+1; a--)
+        {res[a,i] = s; s++;}
+    }
+    return res;
+}
+
+
+void PrintArray(int[,] a)
+{
+    for (int i=0; i<a.GetLength(0); i++)
+    {
+        for (int j=0; j<a.GetLength(1); j++)
+            Console.Write($"|  {a[i, j]}  ");
+        
+        Console.WriteLine("|");
+    }
+}
+
+Console.WriteLine("Введите размерность массива");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите начальное значение");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = GetArray(a,b);
+
+PrintArray(arr);

c1a578f48e55c2b4092bc66a0e51dae2288bf224
Xander2515 <111518086+Xander2515@users.noreply.github.com>
2022-09-19 20:45:57 +0300

    Семинар 8

--- C# Семинар 8/Задача 60/Program.cs
+++ C# Семинар 8/Задача 60/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,,] GetArray(int m, int n, int k)
+{
+    int[,,] result = new int[m, n, k];
+    int a = 10;
+
+    for (int i=0; i<m; i++)
+        for (int j=0; j<n; j++)
+            for (int l = 0; l < k; l++)
+            {
+                result[i, j, l] = a;
+                a++;
+            }
+    
+    return result;
+}
+
+void PrintArray(int[,,] a)
+{
+    for (int i=0; i<a.GetLength(0); i++)
+        for (int j=0; j<a.GetLength(1); j++)
+        {
+            for (int l=0; l<a.GetLength(2); l++)
+                Console.Write($"| {a[i, j, l]} ({i}, {j}, {l}) ");
+        
+            Console.WriteLine("|");
+        }
+}
+
+Console.WriteLine("Введите размерность массива");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+int[,,] arr1 = GetArray(a, b, c);
+
+PrintArray(arr1);

a57664c92c37af915f99b03bd7a6071167f02c4c
Xander2515 <111518086+Xander2515@users.noreply.github.com>
2022-09-20 01:15:39 +0300

    Семинар 8

--- C# Семинар 8/Задача 60/Program.cs
+++ C# Семинар 8/Задача 60/Program.cs
@@ -1,14 +1,21 @@
 ﻿int[,,] GetArray(int m, int n, int k)
 {
     int[,,] result = new int[m, n, k];
+    Random rnd = new Random();
     int a = 10;
 
     for (int i=0; i<m; i++)
         for (int j=0; j<n; j++)
-            for (int l = 0; l < k; l++)
+            for (int l=0; l < k; l++)
             {
+                a=rnd.Next(10,100);
+                for (int ii=0; ii<=i; ii++)
+                    for (int jj=0; jj<=j; jj++)
+                        for (int ll=0; ll<=l; ll++)
+                                if (result[ii,jj,ll]==a)
+                                {a=rnd.Next(10,100); ii=0; jj=0; ll=0;}
+                
                 result[i, j, l] = a;
-                a++;
             }
     
     return result;

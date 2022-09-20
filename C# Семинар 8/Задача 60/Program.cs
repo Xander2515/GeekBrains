32da2c29bb93f5dff3d041775960c1cedf745513
Xander2515 <111518086+Xander2515@users.noreply.github.com>
2022-09-20 07:41:34 +0300

    Семинар 8

--- C# Семинар 8/Задача 60/Program.cs
+++ C# Семинар 8/Задача 60/Program.cs
@@ -2,7 +2,10 @@
 {
     int[,,] result = new int[m, n, k];
     Random rnd = new Random();
-    int a = 10;
+    int a;
+
+    if (m*n*k>90)
+    return result;
 
     for (int i=0; i<m; i++)
         for (int j=0; j<n; j++)
